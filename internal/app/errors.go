b29644b4e6ccf938867b2278de65b39adb8249f5
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-05-29 13:02:31 -0300

    feat: add error messages

---
+++ internal/app/errors.go
@@ -0,0 +1,13 @@
+package app
+
+type errorInfo string
+
+func (e errorInfo) Error() string {
+	return string(e)
+}
+
+const ErrInvalidOperationType = errorInfo("operation type is invalid")
+const ErrDocumentNumberIsInvalid = errorInfo("document number is invalid")
+const ErrInvalidAmount = errorInfo("operation's amount is invalid")
+const ErrAccountNotFound = errorInfo("account not found")
+const ErrAccountAlreadyExists = errorInfo("there is already an account with the given document number")
