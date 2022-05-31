4bc41e4c2fd574850e902e5bd010075716c51f86
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-05-31 00:59:42 -0300

    feat: add initial config

---
+++ configs/config.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     int    `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found to loading")
+	}
+
+	viper.AutomaticEnv()
+
+	configs := Config{
+		DBHost:     viper.GetString("DB_HOST"),
+		DBPort:     viper.GetInt("DB_PORT"),
+		DBUser:     viper.GetString("DB_USER"),
+		DBPassword: viper.GetString("DB_PASSWORD"),
+		DBName:     viper.GetString("DB_NAME"),
+	}
+
+	return configs
+}
