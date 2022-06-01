9d61f35fb66614501b9ae04909298662eef24133
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-06-01 04:48:20 -0300

    feat: update config envs

--- configs/config.go
+++ configs/config.go
@@ -2,17 +2,22 @@ package configs
 
 import (
 	"log"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	DBHost     string `mapstructure:"DB_HOST"`
-	DBPort     int    `mapstructure:"DB_PORT"`
-	DBUser     string `mapstructure:"DB_USER"`
-	DBPassword string `mapstructure:"DB_PASSWORD"`
-	DBName     string `mapstructure:"DB_NAME"`
+	HostName        string        `mapstructure:"HOST_NAME"`
+	HostPort        string        `mapstructure:"HOST_PORT"`
+	GracefulTimeout time.Duration `mapstructure:"GRACEFUL_TIMEOUT"`
+	LogLevel        string        `mapstructure:"LOG_LEVEL"`
+	DBHost          string        `mapstructure:"DB_HOST"`
+	DBPort          int           `mapstructure:"DB_PORT"`
+	DBUser          string        `mapstructure:"DB_USER"`
+	DBPassword      string        `mapstructure:"DB_PASSWORD"`
+	DBName          string        `mapstructure:"DB_NAME"`
 }
 
 func Load() Config {
@@ -21,13 +26,20 @@ func Load() Config {
 	}
 
 	viper.AutomaticEnv()
+	viper.SetDefault("HOST_PORT", "3000")
+	viper.SetDefault("GRACEFUL_TIMEOUT", "15s")
+	viper.SetDefault("LOG_LEVEL", "info")
 
 	configs := Config{
-		DBHost:     viper.GetString("DB_HOST"),
-		DBPort:     viper.GetInt("DB_PORT"),
-		DBUser:     viper.GetString("DB_USER"),
-		DBPassword: viper.GetString("DB_PASSWORD"),
-		DBName:     viper.GetString("DB_NAME"),
+		HostName:        viper.GetString("HOST_NAME"),
+		HostPort:        viper.GetString("HOST_PORT"),
+		GracefulTimeout: viper.GetDuration("GRACEFUL_TIMEOUT"),
+		LogLevel:        viper.GetString("LOG_LEVEL"),
+		DBHost:          viper.GetString("DB_HOST"),
+		DBPort:          viper.GetInt("DB_PORT"),
+		DBUser:          viper.GetString("DB_USER"),
+		DBPassword:      viper.GetString("DB_PASSWORD"),
+		DBName:          viper.GetString("DB_NAME"),
 	}
 
 	return configs
