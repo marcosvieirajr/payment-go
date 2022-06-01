081a9c6211e54e4210804020d82816d170f5534e
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-06-01 01:59:06 -0300

    feat: add transaction repository

---
+++ internal/platform/repository/transaction.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/marcosvieirajr/payment/internal/app/usecases/dto"
+)
+
+type TransactionRepository interface {
+	CreateTransaction(ctx context.Context, t dto.Transaction) (*int64, error)
+}
+
+type transaction struct {
+	db *sql.DB
+}
+
+func NewTransactionRepository(db *sql.DB) *transaction {
+	return &transaction{db}
+}
+
+func (rep *transaction) CreateTransaction(ctx context.Context, t dto.Transaction) (*int64, error) {
+	var id int64
+	sql := `insert into transactions (account_id, operation_type_id, amount, created_from) values ($1, $2, $3, $4) returning id;`
+	err := rep.db.QueryRow(sql, t.Account.ID, t.OperationType, t.Amount, t.CreatedFrom).Scan(&id)
+	if err != nil {
+		return nil, fmt.Errorf("cannot insert transaction: %v", err)
+	}
+	return &id, nil
+}
