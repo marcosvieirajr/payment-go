f3cafb3f03edc3468be12344cce394350d3fd501
Marcos Vieira Jr <13818801+marcosvieirajr@users.noreply.github.com>
2022-06-01 19:33:24 -0300

    feat: add graceful shutdown to the server

--- cmd/restapp/main.go
+++ cmd/restapp/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -70,10 +74,36 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
-	// starting HTTP server
-	log.Infof("listening and serving HTTP on %s", address)
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("error starting server: %s", err)
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		log.Infof("listening and serving HTTP on %s", address)
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error starting server: %s", err)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	wait := make(chan os.Signal, 1)
+	signal.Notify(wait, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received.
+	sig := <-wait
+	log.Infof("received signal %v. Shutting down server gracefully ...", sig.String())
+	log.Info("press Ctrl+C again to force")
+
+	// Create a deadline to wait for. waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), configs.GracefulTimeout)
+	defer cancel()
+
+	// gracefully shutdown the server doesn't block if no connections,
+	// but will otherwise wait until the timeout deadline.
+	srv.Shutdown(ctx)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server forced to shutdown: %v", err)
 	}
 
+	log.Info("server exiting")
+	os.Exit(0)
 }
